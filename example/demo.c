92c2822f6b52a7004a1cc72c3e7d0a1689a4fcc6
ShurupovIlya <ilusha.main@gmail.com>
2021-12-04 17:12:15 +0300

    static

--- example/demo.c
+++ example/demo.c
@@ -10,7 +10,6 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-
 #ifdef _MSC_VER
 #define snprintf _snprintf
 #elif !defined(__MINGW32__)
--- example/example_gl3.c
+++ example/example_gl3.c
@@ -16,6 +16,7 @@
 // 3. This notice may not be removed or altered from any source distribution.
 //
 
+//#define GLFW_INCLUDE_GLEXT
 #define NANOVG_GLEW
 
 #include <stdio.h>
